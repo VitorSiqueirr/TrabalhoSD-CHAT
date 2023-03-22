31797866f872efd9c5a2fb74e72b73479722347a
Vitor Siqueira <41929568+VitorSiqueirr@users.noreply.github.com>
2023-03-22 10:05:35 -0300

    Feito ate 15min

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,20 +1,27 @@
 import React from "react";
+import Add from "../images/addAvatar.png";
 
 const Register = () => {
-  return <div className="formContainer">
-    <div className="formWrapper">
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
         <span className="logo">Marcius Chat</span>
         <span className="title">Registro</span>
         <form>
-            <input type="text" placeholder="nome"/>
-            <input type="email" placeholder="email"/>
-            <input type="password" placeholder="senha"/>
-            <input type="file" />
-            <button>Cadastrar-se</button>
+          <input type="text" placeholder="nome" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="senha" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Adicione um avatar</span>
+          </label>
+          <button>Cadastrar-se</button>
         </form>
         <p>Você já possui uma conta? Logar</p>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Register;
